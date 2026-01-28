80673362eff137244daa655328adae3ac9c2b6e6
Ash-spector <shuklaaniket090@gmail.com>
2026-01-28 12:35:18 +0530

    Add MVC Employee Project

---
+++ Mvc_Project/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Mvc_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Project.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            //Employee employee = new Employee();
+            Employee emp = new Employee()
+            {
+                Empno = 101,
+                Name = "John Doe",
+                Address = "123 Main St",
+                Salary = 50000
+            };
+
+            Employee employee = new Employee { Empno = 102, Name = "Aniket", Address = " GBS socity", Salary = 55000 };
+            return View(employee);
+        }
+    }
+}
