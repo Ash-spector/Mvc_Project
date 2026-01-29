c5a151b5da9eea715298b2e55d75acacfbc5a6c2
Ash-spector <shuklaaniket090@gmail.com>
2026-01-29 12:29:08 +0530

    show the data on display

--- Mvc_Project/Controllers/EmployeeController.cs
+++ Mvc_Project/Controllers/EmployeeController.cs
@@ -10,18 +10,31 @@ namespace Mvc_Project.Controllers
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public ActionResult Index()
+        public ActionResult index()
         {
-            //Employee employee = new Employee();
-            Employee emp = new Employee()
-            {
-                Empno = 101,
-                Name = "John Doe",
-                Address = "123 Main St",
-                Salary = 50000
-            };
+            Employee employee = new Employee();
+            employee.Empno = 101;
+            employee.Name = "Aniket shukla";
+            employee.Address = "GBS";
+            employee.Salary = 1000;
 
-            Employee employee = new Employee { Empno = 102, Name = "Aniket", Address = " GBS socity", Salary = 55000 };
+            Employee employee1 = new Employee();
+            employee.Empno = 102;
+            employee.Name = "Abhay sharma";
+            employee.Address = "Baddu";
+            employee.Salary = 999;
+
+
+            //Employee employee = new Employee { Empno = 101 ,Name = "Aarav", Address = "GBS", Salary = 1000};
+            //Employee emp = new Employee()
+            //{
+            //    Empno = 101,
+            //    Name = "John Doe",
+            //    Address = "123 Main St",
+            //    Salary = 50000
+            //};
+
+            // Employee employee = new Employee { Empno = 102, Name = "Aniket", Address = " GBS socity", Salary = 55000 };
             return View(employee);
         }
     }
