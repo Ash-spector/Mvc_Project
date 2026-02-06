c66a1edee255fe2eb77b4c95c80b6a24b6167a35
Ash-spector <shuklaaniket090@gmail.com>
2026-02-06 21:23:31 +0530

    initial commit

--- Mvc_Project/Controllers/StudentController.cs
+++ Mvc_Project/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Mvc_Project.Data;
+using Mvc_Project.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,37 @@ namespace Mvc_Project.Controllers
     public class StudentController : Controller
     {
         private readonly ApplicationDbcontext context;
+
         public StudentController()
         {
             context = new ApplicationDbcontext();
         }
+
         // GET: Student
         public ActionResult Index()
         {
-            var Students = context.Students.ToList();
-            return View(Students);
+            var students = context.Students.ToList();
+            return View(students);
+        }
+
+        [ActionName("Update-insert")]
+        public ActionResult Upsert(int? id)
+        {
+            var student = new Student();
+
+            if (id == null)
+            {
+                return View(student);
+            }
+
+            student = context.Students.Find(id.Value);
+
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(student);
         }
     }
 }
